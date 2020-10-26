f84a7214ea53fb95aac4a485e9d5f26385c27f2d
David Everett <davideverett1989@gmail.com>
2020-10-26 14:35:39 -0500

    Main components in place.

---
+++ src/components/pages/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './Home.scss';
+
+const Home = () => (
+    <div className="Home">
+        <h1>Home</h1>
+    </div>
+);
+
+export default Home;
---
+++ src/components/pages/Main/Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './Main.scss';
+
+const Main = () => (
+    <div className="Main">
+        <h1>Main</h1>
+    </div>
+);
+
+export default Main;
